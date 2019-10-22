b31fb0238ce6134a0a1e62fb599775eb91c0e4c1
Christos Ploutarchou <cploutarchou@gmail.com>
2019-10-22 23:47:38 +0300

    fix sample page url

--- application/views/menu.php
+++ application/views/menu.php
@@ -23,7 +23,7 @@
             <header>
                 <h1><a href="<?php echo site_url(); ?>">Easy Menu Manager</a></h1>
                 <div id="link">
-                    <a class="button red" href="<?php echo base_url() ?>menu_manager/sample" target="_blank">Preview
+                    <a class="button red" href="<?php echo base_url() ?>menu/sample" target="_blank">Preview
                         Menu</a>
                 </div>
             </header>
