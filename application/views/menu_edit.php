ca440302126a699b871feaac17197bb4dbb0c740
Christos Ploutarchou <cploutarchou@gmail.com>
2019-10-26 14:53:11 +0300

    update edit menu item popup windows css

--- application/views/menu_edit.php
+++ application/views/menu_edit.php
@@ -1,31 +1,33 @@
 <h2>Edit Menu Item</h2>
 <form method="post" action="<?php echo site_url('menu/save'); ?>">
-    <p>
+
+    <div class="form-group">
         <label for="edit-menu-title">Title</label>
-        <input type="text" name="title" id="edit-menu-title" value="<?php echo $row->title; ?>">
-    </p>
-    <p>
+        <input required type="text" name="title" id="edit-menu-title" class="form-control" value="<?php echo
+        htmlentities($row->title) ?>">
+    </div>
+    <div class="form-group">
         <label for="edit-menu-url">URL</label>
-        <input type="text" name="url" id="edit-menu-url" value="<?php echo $row->url; ?>">
-    </p>
-<!--    <p>-->
-<!--        <label for="edit-menu-class">Class</label>-->
-<!--        <input type="text" name="class" id="edit-menu-class" value="--><?php //echo $row->class; ?><!--">-->
-<!--    </p>-->
+        <input type="text" name="url" class="form-control" id="edit-menu-url" value="<?php echo $row->url; ?>">
+    </div>
+    <!--    <p>-->
+    <!--        <label for="edit-menu-class">Class</label>-->
+    <!--        <input type="text" name="class" id="edit-menu-class" value="--><?php //echo $row->class; ?><!--">-->
+    <!--    </p>-->
     <?php if ($row->parent_id == 0) : //only top level menu can be moved ?>
-        <p>
+        <div class="form-group">
             <label for="select_group_id">Group</label>
-            <select name="group_id">
+            <select name="group_id" id="select_group_id" class="form-control">
                 <?php foreach ($menu_groups as $group): ?>
                     <option value="<?php echo $group->id; ?>" <?php if ($group->id == $row->group_id) {
-                echo 'selected';
-            } ?>><?php echo $group->title; ?></option>
-    <?php endforeach;
-    ?>
+                        echo 'selected';
+                    } ?>><?php echo $group->title; ?></option>
+                <?php endforeach;
+                ?>
 
             </select>
-        </p>
+        </div>
         <input type="hidden" name="old_group_id" value="<?php echo $row->group_id; ?>">
-<?php endif; ?>
+    <?php endif; ?>
     <input type="hidden" name="menu_id" value="<?php echo $row->id; ?>">
 </form>
