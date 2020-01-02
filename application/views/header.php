326201edd39edd4bf601307942590d6710eab4ba
Christos Ploutarchou <cploutarchou@gmail.com>
2020-01-02 20:03:23 +0200

    download bootstrap 3 files on local and updates urls

--- application/views/header.php
+++ application/views/header.php
@@ -6,15 +6,16 @@
 
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     <!-- Latest compiled and minified CSS -->
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel='icon' href='../../favicon.ico' type='image/x-icon'/ >
+    <link href="<?php echo base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel='icon' href='<?php echo base_url() ?>favicon.ico' type='image/x-icon'
+    / >
     <!--[if lt IE 9]>
 <!--    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>-->
     <![endif]-->
 
     <script>
-        var _BASE_URL = '<?php echo base_url(); ?>';
-        var current_group_id = <?php if (!empty($group_id)) {
+        const _BASE_URL = '<?php echo base_url(); ?>';
+        let current_group_id = <?php if (!empty($group_id)) {
             echo $group_id;
         } ?>;
     </script>
@@ -23,6 +24,6 @@
     <script src="<?php echo base_url(); ?>assets/js/jquery.mjs.nestedSortable.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/menu.js"></script>
     <!-- Latest compiled JavaScript -->
-    <script src="../../assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>>/assets/js/bootstrap.min.js"></script>
 </head>
 
